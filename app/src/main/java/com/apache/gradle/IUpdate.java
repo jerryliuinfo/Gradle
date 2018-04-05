f6a106876b3f02b94afcac7837a496ce3d78d6ea
“刘祥祥” <xiangxiangliu@sf-express.com>
2018-04-05 18:24:07 +0800

    gradle chapter 1-chapter3

---
+++ app/src/main/java/com/apache/gradle/IUpdate.java
@@ -0,0 +1,9 @@
+package com.apache.gradle;
+
+/**
+ * Created by 01370340 on 2018/4/5.
+ */
+
+public interface IUpdate {
+   <T extends UpdateBean> T get();
+}
---
+++ app/src/main/java/com/apache/gradle/UpdateBean.java
@@ -0,0 +1,8 @@
+package com.apache.gradle;
+
+/**
+ * Created by 01370340 on 2018/4/5.
+ */
+
+public class UpdateBean {
+}
