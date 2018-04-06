45341f1436a5bf6f220c706614f928154ce2a702
“刘祥祥” <xiangxiangliu@sf-express.com>
2018-04-06 13:57:25 +0800

    add some gradle example

--- app/src/main/java/com/apache/gradle/MainActivity.java
+++ app/src/main/java/com/apache/gradle/MainActivity.java
@@ -2,12 +2,17 @@ package com.apache.gradle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "BuildConfig.publish_type = "+BuildConfig.publish_type);
+
     }
+
 }
